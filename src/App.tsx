6f4659b9d12190ea4163869bfd800f4f871d3ccb
MikalKotadia <mikal.m.kotadia@gmail.com>
2022-12-04 22:57:25 -0700

    hovers done

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ function App() {
     return (
         <div className="App">
             <div className="nav-bar shadow p-3 mb-5 w-100">
-                <Logo className="logo" fill="white" />
+                <Logo className="nav-item logo" />
                 <div id="nav_dot" className="dot"></div>
-                <div className="nav-items">About</div>
-                <div className="nav-items">Experience</div>
-                <div className="nav-items">Resumé</div>
-                <div className="nav-items">Contact</div>
+                <div className="nav-item">About</div>
+                <div className="nav-item">Experience</div>
+                <div className="nav-item">Resumé</div>
+                <div className="nav-item">Contact</div>
             </div>
             <div className="tester">
                 <button onClick={() => animations.dotExtend()}>click me</button>
