d031f2003a9b213079f74d1f19dd107851d064a8
MikalKotadia <mikal.m.kotadia@gmail.com>
2023-04-23 21:44:56 -0600

    WIP multiple items in carousel logic

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,17 +1,21 @@
 import "./Carousel.scss";
 import { ReactComponent as ScrollIcon } from "../images/scroll.svg";
+
+import { useState } from "react";
 interface CauroselProperties {
     children?: JSX.Element | JSX.Element[];
 }
 
 export default function Carousel(props: CauroselProperties) {
+    const [index, setIndex] = useState(0);
+
     return (
         <div className="carousel">
-            <div className="blurL">
-                <ScrollIcon className="back-arrow"></ScrollIcon>
+            <div id="L-panel" className="blur-panels active clickable">
+                <ScrollIcon id="back-arrow"></ScrollIcon>
             </div>
-            <div className="blurR">
-                <ScrollIcon className="next-arrow"></ScrollIcon>
+            <div id="R-panel" className="blur-panels active clickable">
+                <ScrollIcon id="next-arrow"></ScrollIcon>
             </div>
             {/* <div className="indicators"></div> */}
             <div className="view">{props.children}</div>
